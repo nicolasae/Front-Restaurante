af4e28c445203d27c41d69c9e51fc07537b93716
nicolasae <nicolas.aguirre@utp.edu.co>
2020-10-18 17:30:58 -0500

    arreglando el footer

--- front-ristorante/src/components/sections/Cta.js
+++ front-ristorante/src/components/sections/Cta.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
 import Input from '../elements/Input';
 
-const propTypes = {
-  ...SectionProps.types,
-  split: PropTypes.bool
-}
-
-const defaultProps = {
-  ...SectionProps.defaults,
-  split: false
-}
 
 const Cta = ({
   className,
@@ -56,20 +45,13 @@ const Cta = ({
              Si desea disfrutar de nuestro restaurante
               </h3>
           </div>
-          <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Reserve Aquí">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
-          </div>
+
         </div>
       </div>
     </section>
   );
 }
 
-Cta.propTypes = propTypes;
-Cta.defaultProps = defaultProps;
+
 
 export default Cta;
