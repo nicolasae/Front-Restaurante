b42615c391827db7274d8d190ec61d04bc906055
nicolasae <nicolas.aguirre@utp.edu.co>
2020-10-18 20:44:31 -0500

    se esta implementando el carrousel

--- front-ristorante/src/components/sections/FeaturesSplit.js
+++ front-ristorante/src/components/sections/FeaturesSplit.js
@@ -3,10 +3,50 @@ import classNames from 'classnames';
 import { SectionSplitProps } from '../../utils/SectionProps';
 import SectionHeader from './partials/SectionHeader';
 import Image from '../elements/Image';
+//Para el carousel
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
-import image1 from './../../assets/images/slides-img-01.jpg'
-import image2 from './../../assets/images/slides-img-02.jpg'
+//Imágenes del carousel
+import entrada1 from './../../assets/images/Menu/Entrada_1.png'
+import entrada2 from './../../assets/images/Menu/Entrada_2.png'
+import entrada3 from './../../assets/images/Menu/Entrada_3.png'
+
+import adicional1 from './../../assets/images/Menu/Adicional_1.png'
+import adicional2 from './../../assets/images/Menu/Adicional_2.png'
+import adicional3 from './../../assets/images/Menu/Adicional_3.png'
+
+import bebidacaliente1 from './../../assets/images/Menu/Bebida_Caliente_1.png'
+import bebidacaliente2 from './../../assets/images/Menu/Bebida_Caliente_2.png'
+import bebidacaliente3 from './../../assets/images/Menu/Bebida_Caliente_3.png'
+
+import bebidafria1 from './../../assets/images/Menu/Bebida_Fria_1.png'
+import bebidafria2 from './../../assets/images/Menu/Bebida_Fria_2.png'
+import bebidafria3 from './../../assets/images/Menu/Bebida_Fria_3.png'
+
+import carne1 from './../../assets/images/Menu/Carne_1.png'
+import carne2 from './../../assets/images/Menu/Carne_2.png'
+import carne3 from './../../assets/images/Menu/Carne_3.png'
+
+import ensalada1 from './../../assets/images/Menu/Ensalada_1.png'
+import ensalda2 from './../../assets/images/Menu/Ensalada_2.png'
+import ensalada3 from './../../assets/images/Menu/Ensalada_3.png'
+
+import pescado1 from './../../assets/images/Menu/Pescado_1.png'
+import pescado2 from './../../assets/images/Menu/Pescado_2.png'
+import pescado3 from './../../assets/images/Menu/Pescado_3.png'
+
+import pollo1 from './../../assets/images/Menu/Pollo_1.png'
+import pollo2 from './../../assets/images/Menu/Pollo_2.png'
+import pollo3 from './../../assets/images/Menu/Pollo_3.png'
+
+import postre1 from './../../assets/images/Menu/Postre_1.png'
+import postre2 from './../../assets/images/Menu/Postre_2.png'
+import postre3 from './../../assets/images/Menu/Postre_3.png'
+
+import sopa1 from './../../assets/images/Menu/Sopa_1.png'
+import sopa2 from './../../assets/images/Menu/Sopa_2.png'
+import sopa3 from './../../assets/images/Menu/Sopa_3.png'
+
 
 const propTypes = {
   ...SectionSplitProps.types
@@ -84,9 +124,18 @@ const FeaturesSplit = ({
                 </p>
               </div>
               <div className="App">
-                <AliceCarousel autoPlay autoPlayInterval="1000">
-                  <img src={image1} className="sliderimg" alt="Features split 01" width={100} height={500}/>
-                  <img src={image2} className="sliderimg" alt="Features split 01" width={100} height={500}/>
+                <AliceCarousel
+                  autoPlay = {true}
+                  onResized = {true}
+                  playButtonEnabled={true}
+                  //itemsInSlide= {3}
+                  autoPlayInterval={1500}
+                  >
+
+                  <img src={entrada1} className="sliderimg" alt="Entradas 1"  width="528" height="396" />
+                  <img src={entrada2} className="sliderimg" alt="Entradas 2" width="528" height="396" />
+                  <img src={entrada3} className="sliderimg" alt="Entradas 3" width="528" height="396" />
+
                 </AliceCarousel>
                 </div>
             </div>
@@ -105,18 +154,22 @@ const FeaturesSplit = ({
                   </ol>                
                 </p>
               </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
-                )}
-                data-reveal-container=".split-item">
-                <Image
-                  src={require('./../../assets/images/features-split-image-02.png')}
-                  alt="Features split 02"
-                  width={528}
-                  height={396} />
-              </div>
+              <div className="App">
+                <AliceCarousel
+                  autoPlay = {true}
+                  onResized = {true}
+                  playButtonEnabled={true}
+                  //itemsInSlide= {3}
+                  autoPlayInterval={1500}
+                  buttonsDisabled={true} 
+                  mouseDragEnabled={true}
+                  >
+
+                  <img src={carne1} className="sliderimg" alt="Carne 1"  width="528" height="396" />
+                  <img src={carne2} className="sliderimg" alt="Carne 2" width="528" height="396" />
+                  <img src={carne3} className="sliderimg" alt="Carne 3" width="528" height="396" />
+                </AliceCarousel>
+                </div>
             </div>
             
             <div className="split-item">
